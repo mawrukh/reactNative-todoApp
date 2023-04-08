1d5293a61df61970b90940cb227d94bb7938ad2b
Mahrukh <70944109+mawrukh@users.noreply.github.com>
2023-04-08 12:28:56 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,120 +1,164 @@
-import React, { useState, useEffect } from 'react';
-import {
-  StyleSheet,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Text,
-  FlatList
-} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const TodoApp = () => {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
+export default function TodoApp() {
+  const [todoList, setTodoList] = useState([]);
+  const [todoItem, setTodoItem] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState(null);
+  const [submitButtonLabel, setSubmitButtonLabel] = useState('Add');
 
-  const addTodo = async () => {
-    if (input.length > 0) {
-      const newTodo = {
-        id: Math.random().toString(),
-        value: input
-      };
-      await AsyncStorage.setItem(newTodo.id, newTodo.value);
-      setTodos([...todos, newTodo]);
-      setInput('');
+  // Add a new item to the todoList or update an existing one
+  const handleAddOrUpdateTodo = () => {
+    if (todoItem) {
+      if (selectedTodo) {
+        // Update an existing item
+        handleUpdateTodo();
+      } else {
+        // Add a new item
+        setTodoList([...todoList, { id: Date.now(), title: todoItem, completed: false }]);
+        setTodoItem('');
+      }
     }
   };
 
-  const deleteTodo = async (id) => {
-    await AsyncStorage.removeItem(id);
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
+  // Update the title of an existing todo item
+  const handleUpdateTodo = () => {
+    const updatedList = todoList.map(todo => {
+      if (todo.id === selectedTodo.id) {
+        return { ...todo, title: todoItem };
+      }
+      return todo;
+    });
+    setTodoList(updatedList);
+    setSelectedTodo(null);
+    setTodoItem('');
+    setSubmitButtonLabel('Add');
   };
 
-  const getTodos = async () => {
-    try {
-      const keys = await AsyncStorage.getAllKeys();
-      const items = await AsyncStorage.multiGet(keys);
-      const loadedTodos = items.map((item) => ({
-        id: item[0],
-        value: item[1]
-      }));
-      setTodos(loadedTodos);
-    } catch (error) {
-      console.log(error);
-    }
+  // Remove a todo item from the todoList
+  const handleDeleteTodo = id => {
+    const updatedList = todoList.filter(todo => todo.id !== id);
+    setTodoList(updatedList);
   };
 
-  useEffect(() => {
-    getTodos();
-  }, []);
+  // Set the selectedTodo for editing
+  const handleEditTodo = todo => {
+    setSelectedTodo(todo);
+    setTodoItem(todo.title);
+    setSubmitButtonLabel('Update');
+  };
+
 
   return (
-    <View style={styles.container}>
+  <View style={styles.container}>
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>Add Item</Text>
       <TextInput
         style={styles.input}
-        placeholder="Enter todo"
-        value={input}
-        onChangeText={(text) => setInput(text)}
+        placeholder="Type here to add a new item"
+        onChangeText={text => setTodoItem(text)}
+        value={todoItem}
       />
-      <TouchableOpacity style={styles.button} onPress={addTodo}>
-        <Text style={styles.buttonText}>Add Todo</Text>
+      <TouchableOpacity
+        style={[styles.button, { borderRadius: 20 }]}
+        onPress={handleAddOrUpdateTodo}
+      >
+        <Text style={styles.buttonLabel}>{submitButtonLabel}</Text>
       </TouchableOpacity>
-      <FlatList
-        data={todos}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            style={styles.todo}
-            onPress={() => deleteTodo(item.id)}
-          >
-            <Text style={styles.todoText}>{item.value}</Text>
-          </TouchableOpacity>
-        )}
-        keyExtractor={(item) => item.id}
-      />
     </View>
-  );
-};
+    <View style={styles.listContainer}>
+      <Text style={styles.label}>My List</Text>
+      {todoList.map(todo => (
+        <View key={todo.id} style={styles.todoItemContainer}>
+          <TextInput
+            style={styles.todoTitle}
+            onChangeText={text => setTodoItem(text)}
+            value={selectedTodo && selectedTodo.id === todo.id ? todoItem : todo.title}
+            editable={selectedTodo && selectedTodo.id === todo.id}
+          />
+          <TouchableOpacity
+            style={styles.deleteButton}
+            onPress={() => handleDeleteTodo(todo.id)}
+          >
+            <Icon name="times" size={20} color="#fff" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.editButton}
+            onPress={() => handleEditTodo(todo)}
+          >
+            <Icon name="pencil" size={20} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      ))}
+    </View>
+  </View>
+);
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20
+    padding: 20,
+    marginTop: 50,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
   },
   input: {
-    width: '100%',
-    height: 50,
-    borderWidth: 1,
+    height: 40,
     borderColor: '#ccc',
-    borderRadius: 5,
+    borderWidth: 1,
+    borderRadius: 20,
     paddingHorizontal: 10,
-    marginBottom: 10
+    marginBottom: 10,
   },
   button: {
-    backgroundColor: 'blue',
-    padding: 10,
+    backgroundColor: '#007AFF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
     borderRadius: 5,
-    marginBottom: 10
+    
+     marginBottom: 50,
   },
-  buttonText: {
+  buttonLabel: {
     color: '#fff',
-    fontWeight: 'bold'
+    fontSize: 18,
+    textAlign: 'center',
   },
-  todo: {
-    backgroundColor: '#eee',
+  listContainer: {
+    flex: 1,
+  },
+  todoItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f2f2f2',
     padding: 10,
     borderRadius: 5,
-    marginBottom: 10
+    marginBottom: 10,
   },
-  todoText: {
-    color: 'black',
-    fontWeight: 'bold'
-  }
+  todoTitle: {
+    flex: 1,
+    fontSize: 16,
+    color: '#333',
+    paddingHorizontal: 10,
+  },
+  deleteButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  editButton: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+  },
 });
-
-export default TodoApp;
-
-
-
